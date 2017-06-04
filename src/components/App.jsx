e6d1f543e8a3a32fde0a2196c003af8ab1efff82
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-04 02:42:10 +0000

    adding components of the main view and initializing state in the main component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
+// Most of the logic and programming will go here: fetching things from the API, handling events...
+
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'Archer09',
+      userData: [],
+      userRepos: [],
+      perPage: 5
+    }
+  }
   render () {
     return (
       <div>
