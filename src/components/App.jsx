fb9882a1905ecc6bad28715f74ffe8a798c7c01e
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-04 08:01:40 +0000

    finishing the profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'Archer07',
+      username: 'john',
       userData: {},
       userRepos: [],
       perPage: 5,
--- src/components/github/Profile.jsx
+++ src/components/github/Profile.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 class Profile extends Component {
   render() {
     return (
+      <div className="row">
       <div className="col-md-3">
         <div className="panel panel-primary">
           <div className="panel-heading">
@@ -17,6 +18,30 @@ class Profile extends Component {
         <div className="well">
           <p>{this.props.userData.bio}</p>
         </div>
+      </div>
+      <div className="col-md-7">
+        <div className="panel panel-primary">
+          <div className="panel-heading">
+            <h3 className="panel-title text-center">User Info</h3>
+          </div>
+          <div className="panel-body">
+            <div  className="row">
+              <span className="label label-default">Repos: {this.props.userData.public_repos}</span>
+              <span className="label label-primary">Gists: {this.props.userData.public_gists}</span>
+              <span className="label label-success">Followers: {this.props.userData.followers}</span>
+              <span className="label label-warning">Following: {this.props.userData.following}</span>
+            </div>
+            <div className='row'>
+            <ul className="list-group col-md-10">
+              <li className="list-group-item"><strong>Username: </strong> {this.props.userData.login} </li>
+              <li className="list-group-item"><strong>Location: </strong> {this.props.userData.location} </li>
+              <li className="list-group-item"><strong>Email Address: </strong> {this.props.userData.email || 'None'} </li>
+              </ul>
+            </div>
+            <a href={this.props.userData.html_url} className="btn btn-success" target="_blank">Visit Profile</a>
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
